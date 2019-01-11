dbeccdcb633638203da52f06e13a1e4779b992e8
Alicia Baker <aliciabaker314@gmail.com>
2019-01-11 06:25:47 -0700

    add user and stock association and views

--- app/models/user_stock.rb
+++ app/models/user_stock.rb
@@ -1,4 +1,12 @@
 class UserStock < ApplicationRecord
   belongs_to :user
   belongs_to :stock
+
+  def destroy
+    stock = Stock.find(params[:id])
+    @user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    @user_stock.destroy
+    flash[:notice] = "Stock was successfully removed from portfolio"
+    redirect_to my_portfolio_path
+  end
 end
